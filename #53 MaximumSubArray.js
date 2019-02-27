c8127548ace026bad4bc2fa446e02f4dd96483f0
HSGill <harjotgill73@gmail.com>
2019-02-28 05:35:02 +1300

    Solutin O(n^2)

--- #53 MaximumSubArray.js
+++ #53 MaximumSubArray.js
@@ -3,14 +3,29 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let max = 0;
-    for(let i=0;i<nums.length;i++){
-        let maxStart = 0;
-        for(let j=i;j<nums.length;j++){
-            maxStart += nums[j];
-            max = Math.max(max,maxStart);
-        }
-    }
-    return console.log(max);
+  let max =-Number.MAX_VALUE ;
+  for(let i=0;i<nums.length;i++){
+      let maxStart = 0;
+      for(let j=i;j<nums.length;j++){
+          maxStart += nums[j];
+       max = Math.max(max,maxStart);
+      }
+  }
+ return max;
 }     
-maxSubArray([1, -2, 3, 4, -9, 6]);
+
+/* function getMaxSubSum(arr) {
+    let maxSum = 0;
+    let partialSum = 0;
+  
+    for (let item of arr) { // for each item of arr
+      partialSum += item; // add it to partialSum
+      maxSum = Math.max(maxSum, partialSum); // remember the maximum
+      if (partialSum < 0) partialSum = 0; // zero if negative
+    }
+  
+    return console.log(maxSum);
+  }
+   */
+  
+  maxSubArray([-1, -2, -3, -4, -9, -6]);
