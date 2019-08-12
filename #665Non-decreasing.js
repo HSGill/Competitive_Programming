cda6d77841c9e8faa9f81a30444d22b9e683bd01
HSGill <harjotgill73@gmail.com>
2019-08-12 21:56:39 +1200

    non decreasing array

--- #665Non-decreasing.js
+++ #665Non-decreasing.js
@@ -2,25 +2,25 @@
 * @param {number[]} nums
 * @return {boolean}
 */
-var checkPossibility = function(nums) {
-   let count =0;
-   for (i=0;i<nums.length-1;i++){
+var checkPossibility = function (nums) {
+    let count = 0;
+    for (i = 0; i < nums.length - 1 ; i++) {
        if(nums[i]>nums[i+1]){
-            if(i>=0){
-                if(nums[i-1]<=nums[i+1]){
-                    nums[i] = nums[i-1];
-                }
-                else{
-                    nums[i+1] = nums[i];
-
-                }
-                count++;
-                if(count>1){
-                    return false;
-                }
-            }
-            return true;
+           if(i>0){
+               if(nums[i-1] > nums[i+1]){
+                   nums[i+1] = nums[i]
+               }
+               else{
+                   nums[i] = nums[i-1];
+               }
+           }
+           count++;
        }
-   }
+    }
+    if(count<=1){
+        return true;
+    } 
+    else return false;
+    
 };
-console.log(checkPossibility([4,2,1]));
+console.log(checkPossibility([3,4,2,3]));
