fd7205b78db9f051e46d5cc814992f4dbad6d95e
HSGill <harjotgill73@gmail.com>
2020-01-05 20:51:53 +1300

    #66 PlusOne

--- #230 kth smallest Element.js
+++ #230 kth smallest Element.js
@@ -10,19 +10,27 @@
  * @param {number} k
  * @return {number}
  */
+class Node {
+    constructor(value){
+      this.left = null;
+      this.right = null;
+      this.value = value;
+    }
+  }
+  
 var kthSmallest = function(root, k) {
-   let currentNode = this.root;
-   while(currentNode){
-       if(k<currentNode.value){
-           currentNode = currentNode.left;
+    let stack =[];
+    
+   while( root){
+       while(root !=null){
+           stack.push(root);
+           root = root.left;
        }
-       else if(k>currentNode.value){
-           currentNode = currentNode.right;
-       }
-       else if(currentNode = k){
-           return currentNode;
-       }
-   } 
+       root = stack.pop();
+           k--;
+           if(k==0)return root.val;
+           root = root.right;
+   }
 };
 
 console.log(kthSmallest([41,37,44,24,39,42,48,1,35,38,40,null,43,46,49,0,2,30,36,null,null,null,null,null,null,45,47,null,null,null,null,null,4,29,32,null,null,null,null,null,null,3,9,26,null,31,34,null,null,7,11,25,27,null,null,33,null,6,8,10,16,null,null,null,28,null,null,5,null,null,null,null,null,15,19,null,null,null,null,12,null,18,20,null,13,17,null,null,22,null,14,null,null,21,23],
